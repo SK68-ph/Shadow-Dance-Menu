0517e96e5594ed7a300a15ac69dbdff148de3269
Martin S <35738176+skrixx68@users.noreply.github.com>
2022-06-24 00:50:41 +0800

    clean

--- Dota2 ImGui DX11/utilities.h
+++ Dota2 ImGui DX11/utilities.h
@@ -7,4 +7,5 @@ namespace utilities {
 	std::uintptr_t ReadMultiLevelPointer(uintptr_t ptr, std::vector<uint32_t>& offsets);
 	bool isPtrReadable(uintptr_t base);
 	void* GetInterface(const char* dllname, const char* interfacename);
+
 }
